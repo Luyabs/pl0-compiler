56c1e61d7d4404aae51db3f35923ecd84e1331e6
sunhanyang <928837057@qq.com>
2023-04-18 01:26:17 +0800

    增加条件分析parser_condition

--- parser_.py
+++ parser_.py
@@ -10,8 +10,8 @@ class Parser:
         self.lexing_result = lexing_result
         self.get_next_token()
 
-        while self.current < len(self.lexing_result):   # 每一句表达式结束 就开始判断下一句是否为表达式
-            success = self.parse_expression()
+        while self.current < len(self.lexing_result):  # 每一句表达式结束 就开始判断下一句是否为表达式
+            success = self.parse_condition()
             if self.current > len(self.lexing_result):
                 self.parsing_result.append(['failed', 'EOF', 'EOF'])
             else:
@@ -27,6 +27,14 @@ class Parser:
         self.token = self.lexing_result[self.current] if self.current < len(self.lexing_result) else ['EOF', 'EOF']
         # 获取新token 最后一词则设为EOF
 
+    # 条件分析
+    def parse_condition(self) -> bool:  # <条件> ::=<表达式>[=|#|<|>|<=|>=]<表达式>
+        success = self.parse_expression()  # <表达式>
+        while self.is_compare_operator():  # <比较运算符>
+            self.get_next_token()
+            success = success and self.parse_expression()
+        return success
+
     # 表达式 分析
     def parse_expression(self) -> bool:  # <表达式> ::= [+|-]<项>{<加法运算符> <项>}
         if self.is_add_operator():  # [+|-]
@@ -40,22 +48,22 @@ class Parser:
         return success
 
     # 项 分析
-    def parse_term(self) -> bool:   # <项> ::= <因子>{<乘法运算符> <因子>}
-        success = self.parse_factor()   # <因子>
+    def parse_term(self) -> bool:  # <项> ::= <因子>{<乘法运算符> <因子>}
+        success = self.parse_factor()  # <因子>
 
         while self.is_multiply_operator():  # {<乘法运算符> ...}
             self.get_next_token()
-            success = success and self.parse_factor()   # <因子>
+            success = success and self.parse_factor()  # <因子>
 
         return success
 
     # 因子 分析
     def parse_factor(self) -> bool:  # <因子> ::= <标识符>|<无符号整数>| '('<表达式>')'
-        if self.is_ident() or self.is_number():     # <标识符>|<无符号整数>
+        if self.is_ident() or self.is_number():  # <标识符>|<无符号整数>
             self.get_next_token()
             return True
 
-        elif self.token[0] == 'lparen':     # | '('<表达式>')'
+        elif self.token[0] == 'lparen':  # | '('<表达式>')'
             self.get_next_token()
             self.parse_expression()
             if self.token[0] == 'rparen':
@@ -84,3 +92,8 @@ class Parser:
     def is_number(self) -> bool:
         pos, content = self.token
         return pos == 'number'
+
+    # 比较运算符
+    def is_compare_operator(self) -> bool:
+        pos, content = self.token
+        return pos == 'eql' or pos == 'neq' or pos == 'lss' or pos == 'leq' or pos == 'gtr' or pos == 'geq'
